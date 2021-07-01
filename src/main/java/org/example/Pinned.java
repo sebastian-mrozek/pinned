71783336146878a8f9d1ecac67630dc5419e7504
sebastian-mrozek <sebastian.mrozek@gmail.com>
2021-07-01 18:49:33 +1200

    Add implementation

--- src/main/java/org/example/Pinned.java
+++ src/main/java/org/example/Pinned.java
@@ -1,10 +1,31 @@
 package org.example;
 
 import java.util.Collection;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Pinned {
 
-    public Collection<Short> generate() {
-        return null;
+    private final Random random = new Random();
+
+    public Collection<String> generate() {
+        return generate(1000);
+    }
+
+    private Collection<String> generate(int count) {
+        Set<String> elements = new TreeSet<>();
+        while (elements.size() != count) {
+            int required = count - elements.size();
+            for (int i = 0; i < required; i++) {
+                elements.add(nextRandomPin());
+            }
+        }
+        return elements;
+    }
+
+    private String nextRandomPin() {
+        return String.format("%4d", random.nextInt(10000) - 1)
+                .replace(' ', '0');
     }
 }
