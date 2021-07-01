6a7cc1a372553bb538388369eda250b5b675bb71
sebastian-mrozek <sebastian.mrozek@gmail.com>
2021-07-01 18:39:14 +1200

    Update tests
    Check each pin is 4-digit.
    Change generated collection to be typed with String.

--- src/test/java/org/example/PinnedTest.java
+++ src/test/java/org/example/PinnedTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PinnedTest {
 
@@ -14,15 +14,22 @@ public class PinnedTest {
 
     @Test
     public void testReturns1000Elements() {
-        Collection<Short> elements = service.generate();
+        Collection<String> elements = service.generate();
         assertNotNull(elements);
         assertEquals(1000, elements.size(), "Collection count");
     }
 
     @Test
     public void testElementsAreUnique() {
-        Collection<Short> elements = service.generate();
+        Collection<String> elements = service.generate();
         assertEquals(1000, new HashSet<>(elements).size());
+    }
+
+    @Test void testElementsAre4Digits() {
+        Collection<String> failed = service.generate().stream()
+                .filter(pin -> pin.length() != 4)
+                .collect(Collectors.toList());
+        assertTrue(failed.isEmpty(), "There are " + failed.size() + " pin numbers that are not 4-digit: " + failed.toString());
     }
 
 }
